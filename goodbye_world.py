d4176217928ea2b19da1b949947d28406d993a82
safakoral <safakoral@gmail.com>
2023-12-04 00:16:40 +0300

    goodbye_first

--- goodbye_world.py
+++ goodbye_world.py
@@ -1,20 +1,9 @@
 from flask import Flask
-
-
-
 app = Flask(__name__)
-
-
-
-@app.route('/bye')
-
+@app.route('/')
 def goodbye__world():
-
     return 'Goodbye, World!'
-
-
 
 if __name__ == '__main__':
 
-    app.run(debug=True)
-
+    app.run(debug=True, host="0.0.0.0", port="5000")
